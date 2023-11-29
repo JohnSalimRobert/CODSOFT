8e82724aec3d3d874e3bd2f4569bfb2ec420e1c5
JohnSalimRobert <121552921+JohnSalimRobert@users.noreply.github.com>
2023-11-29 15:35:13 +0500

    added Main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import MainComponent from "./components/MainComponent";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <MainComponent />
     </div>
   );
 }
