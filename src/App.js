0786d9e58d67b849ee97ab2cc12b3d942adaae75
JohnSalimRobert <121552921+JohnSalimRobert@users.noreply.github.com>
2023-12-04 23:14:02 +0500

    Added a newsletter component and cards component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import MainComponent from "./components/MainComponent";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Hero />
       <MainComponent />
+      <Newsletter />
+      <Cards />
     </div>
   );
 }
