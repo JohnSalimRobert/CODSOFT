6060cf1767e906d9e4b70d9cce33e508e2ba7953
JohnSalimRobert <121552921+JohnSalimRobert@users.noreply.github.com>
2023-12-05 13:35:58 +0500

    footer section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import MainComponent from "./components/MainComponent";
 import Newsletter from "./components/Newsletter";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <MainComponent />
       <Newsletter />
       <Cards />
+      <Footer />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Typed from 'react-typed'
+import backgroundimg from '../assests/background-img.png'
 
 const Hero = () => {
   return (
     <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p className='text-[#1E5128] font-bold p-2'>Cloud Game</p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 text-[#D8E9A8]'>Meta Game Pass</h1>
+        <div className=' max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <h1 className='text-[#1E5128] md:text-7xl sm:text-6xl text-4xl font-bold p-2'>Cloud Game</h1>
         <div className='flex justify-center items-center'>
             <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4 text-[#D8E9A8]'>Be the first to play at your</p>
             <Typed
@@ -17,7 +17,8 @@ const Hero = () => {
             loop/>
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>Enjoy hundreds of high-quality games with friends on console, PC, and cloud.</p>
-        <button className='bg-[#4E9F3D] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+        <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold md:py-6 pt-4 text-[#D8E9A8]'>Meta Game Pass</h1>
+        <button className='bg-[#4E9F3D] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Join Now</button>
         </div>
     </div>
   )
